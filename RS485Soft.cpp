1fbc34cff2b94cf23fc10b6280223c91604e98aa
Rafael Dalzotto <rdalzotto@itba.edu.ar>
2022-07-06 05:33:38 -0300

    new version

--- RS485Soft.cpp
+++ RS485Soft.cpp
@@ -1,53 +1,34 @@
 #include "RS485Soft.h"
 
-/*
- *
- */
-RS485Soft::RS485Soft(SoftwareSerial &softSerial, uint8_t rxPin, uint8_t txPin, uint8_t txControl)
+RS485Soft::RS485Soft(uint8_t rxPin, uint8_t txPin, uint8_t txControl) : SoftwareSerial(rxPin, txPin)
 {
-	rs485 = &softSerial;
 	this->rxPin = rxPin;
 	this->txPin = txPin;
 	this->txControl = txControl;
-	size = 0;
 }
 
 RS485Soft::~RS485Soft()
 {
-	rs485->stopListening();
-	rs485->end();
+	SoftwareSerial::stopListening();
+	SoftwareSerial::end();
 }
 
-// Send null-terminated string  NULL terminator is NOT INCLUDED in the packet !!!!  (!)
-void RS485Soft::sendChunk(const char *data)
+void RS485Soft::send(RS485Packet& packet)
 {
 	txMode();
-	// Header
+
+	// Sync Header
 	write(ASCII_NUL);
 	write(ASCII_SOH);
 	write(ASCII_STX);
+	write(packet.size);
 
-	for (uint8_t i = 0; i < RS485_SOFT_BUFFER_SIZE && (data[i] != (char)0); i++)
-		write((uint8_t)data[i]); // body
+	for (int i = 0; i < packet.size && i < RS485_MAX_DATA_SIZE; i++)
+		write(packet.data[i]);	
 
-	// footer
-	write(ASCII_ETX);
-	write(ASCII_ETB); // 23 = 16 + 4 + 2 + 1 = 0001 0111 = 1 7
-}
-
-// Send n bytes of data
-void RS485Soft::sendChunk(uint8_t *data, uint8_t n)
-{
-	txMode();
-	// Header
+	// Footer
+	write(packet.hash());
 	write(ASCII_NUL);
-	write(ASCII_SOH);
-	write(ASCII_STX);
-	for (uint8_t i = 0; i < n && i < RS485_SOFT_BUFFER_SIZE; i++)
-		write(data[i]); // body
-	// footer
-	write(ASCII_ETX);
-	write(ASCII_ETB);
 }
 
 void RS485Soft::_timeStamp()
@@ -64,25 +45,8 @@ uint8_t RS485Soft::_timedOut()
 	return (millis() - timestamp) > timeout;
 }
 
-
-
-/* Read chuck of data
- * @retval 0 = NEW_PACKET
- * @retval 1 = NULL_PACKET
- * @retval 2 = ERROR_TIMEOUT
- * @retval 3 = ERROR_EXCEEDED_NULL_COUNT
- * @retval 4 = ERROR_INCOMPLETE_OR_BROKEN
- * @retval 5 = ERROR_OVERFLOW
- * @retval 6 = ERROR_EMPTY
- * @retval 7 = ERROR_UNKNOWN
- */
-read_code_t RS485Soft::readChunk()
+bool RS485Soft::readPacket(RS485Packet& packet)
 {
-	rxMode();
-	read_fsm_state_t state = FSM_WAIT_H0;
-	read_code_t out = ERROR_UNKNOWN;
-	size = 0;
-
 	#ifdef RS485_DEBUG
 	#ifdef RS485_DEBUG_TIMESTAMP
 	Serial.print("t: ");
@@ -96,120 +60,102 @@ read_code_t RS485Soft::readChunk()
 
 	#endif
 
+	rxMode();
 	_timeStamp();
+	packet.clear();
 
+	uint8_t i = 0; // size counter
+
+	uint8_t state = FSM_WAIT_H0;
 	while (state != FSM_END)
 	{
-		uint16_t b = ASCII_EMPTY_VALUE; // this value will be ignored
-
-		if (rs485->available())
-		{
-			b = read(); // read one byte at a time
-
-			#ifdef RS485_DEBUG
-			debug_output[debug_output_count++] = (uint8_t)b;
-			#endif
-		}
-
 		if (_timedOut())
 		{
-			out = ERROR_TIMEOUT;
-			state = FSM_END;
+			packet.error = ERROR_TIMEOUT;
+			return false;
 		}
 
-		switch (state) // FINITE STATE MACHINE
+		if (SoftwareSerial::available())
 		{
-		case FSM_WAIT_H0: 
-			if (b == ASCII_SOH) // First byte header
-			{
-				state = FSM_WAIT_H1; // OK, next state
-				_timeStamp();
-			}
-			if (b == ASCII_NUL) // Ignore first NULL bytes
-			{
-				out = NULL_PACKET;
-				state = FSM_END;
-			}
-			break;
+			uint8_t b = read(); // read one byte at a time
 
-		case FSM_WAIT_H1:
-			if (b == ASCII_STX) // Second byte header
-			{
-				state = FSM_PACKET; // OK, next state
-				_timeStamp();
-			}
-			else if (b != ASCII_EMPTY_VALUE)
-			{
-				state = FSM_END;
-				out = ERROR_INCOMPLETE_OR_BROKEN;
-			}
-			break;
+			#ifdef RS485_DEBUG
+			debug_output[debug_output_count++] = b;
+			#endif
 
-		case FSM_PACKET:
-			switch (b)
+			switch (state) // FINITE STATE MACHINE
 			{
-			case ASCII_EMPTY_VALUE:
-			break;
-			case ASCII_ETX:
-			{
-				if (size == 0)
+			case FSM_WAIT_H0: 
+				switch (b)
 				{
-					out = ERROR_EMPTY;
-					state = FSM_END;
-				}
-				else
-				{
+				case ASCII_SOH: // First byte header
 					_timeStamp();
-					state = FSM_WAIT_F1;
-				}
-			}
-			break;
-			case ASCII_ETB:
-			{
-				state = FSM_END;
-				out = ERROR_INCOMPLETE_OR_BROKEN;
-			}
-			break;
-			default:
-				if (size < RS485_SOFT_BUFFER_SIZE)
-				{
-					_timeStamp();
-					buffer[size] = b;
-					size++;
-				}
-				else
-				{
-					state = FSM_END;
-					out = ERROR_OVERFLOW;
+					state = FSM_WAIT_H1; // OK, next state
+					break;
+				case ASCII_NUL: // Ignore first NULL bytes
+					packet.error = NULL_PACKET;
+					return false;				
+					break;
 				}
 				break;
-			}
-			break;
 
-		case FSM_WAIT_F1:
-			if (b == ASCII_ETB)
-			{
-				state = FSM_END; // OK
-				out = NEW_PACKET;	 // OK Packet Complete!
-				// flush
-				delay(10);
-				read();
-				read();
-			}
-			else if (b != ASCII_EMPTY_VALUE)
-			{
+			case FSM_WAIT_H1:
+				if(b == ASCII_STX) // Second byte header
+				{
+					_timeStamp();
+					state = FSM_SIZE; // OK, next state
+				}
+				else
+				{
+					packet.error = ERROR_HEADER_BROKEN;
+					return false;
+				}
+				break;
+
+			case FSM_SIZE:
+				packet.size = b;
+				if(packet.size <= RS485_MAX_DATA_SIZE)
+					state = FSM_PACKET; // Ok next
+				else
+				{
+					packet.error = ERROR_SIZE_OVERFLOW;
+					return false;
+				}
+				break;
+
+			case FSM_PACKET:
+				packet.data[i++] = b;
+				if( i >= packet.size || i >= RS485_MAX_DATA_SIZE)
+					state = FSM_HASH;
+
+				break;
+
+			case FSM_HASH:
+				if(packet.hash() == b)
+				{
+					state = FSM_END;
+					packet.error = PACKET_OK;
+					delay(10);
+					SoftwareSerial::read();
+					SoftwareSerial::read();
+					return true;
+				}
+				else
+				{
+					packet.error = ERROR_HASH_MISMATCH;
+					return false;
+				}
+				break;
+
+			case FSM_END:
+				break;
+
+			default:
+				packet.error = ERROR_UNKNOWN;
 				state = FSM_END;
-				out = ERROR_INCOMPLETE_OR_BROKEN;
+				return false;
+				break;
 			}
-			break;
-
-		case FSM_END:
-			break;
-
-		default:
-			out = ERROR_UNKNOWN;
-			state = FSM_END;
-			break;
 		}
 	}
 
@@ -240,78 +186,14 @@ read_code_t RS485Soft::readChunk()
 	Serial.println("\n");
 #endif
 
-	errorCode = out;
-	return out;
-}
-
-/* 
- * Returns error codes
- * @retval 0  = No errors (OK)
- * @retval 2 = ERROR_TIMEOUT
- * @retval 3 = ERROR_EXCEEDED_NULL_COUNT
- * @retval 4 = ERROR_INCOMPLETE_OR_BROKEN
- * @retval 5 = ERROR_OVERFLOW
- * @retval 6 = ERROR_EMPTY
- * @retval 7 = ERROR_UNKNOWN
- */
-uint8_t RS485Soft::error()
-{
-	switch (errorCode)
-	{
-	case NULL_PACKET:
-	case NEW_PACKET:
-		errorCode = 0; // no errors
-		break;
-	default:
-		break;
-	}
-	return errorCode;
-}
-
-void RS485Soft::printChunk()
-{
-	for (int i = 0; i < size && i < RS485_SOFT_BUFFER_SIZE; i++)
-		Serial.print((char)buffer[i]);
-	Serial.println();
-}
-
-void RS485Soft::clearChunk()
-{
-	size = 0; // we dont need to erase the buffer
-}
-
-uint8_t RS485Soft::chunkSize()
-{
-	return size;
-}
-
-uint8_t RS485Soft::chunkData(uint8_t i)
-{
-	if (i < size && i < RS485_SOFT_BUFFER_SIZE)
-		return buffer[i];
-	return 0;
+	return false;
 }
 
 void RS485Soft::begin(long baudrate)
 {
-	begin(baudrate, RS485_DEFAULT_TIMEOUT);
-}
-
-void RS485Soft::begin(long baudrate, unsigned long timeout)
-{
-	rs485->begin(baudrate);
+	SoftwareSerial::begin(baudrate);
 	this->timeout = timeout;
 	pinMode(txControl, OUTPUT);
-}
-
-void RS485Soft::write(uint8_t b)
-{
-	rs485->write(b);
-}
-
-uint8_t RS485Soft::read()
-{
-	return (uint8_t)rs485->read();
 }
 
 void RS485Soft::txMode()
@@ -327,5 +209,5 @@ void RS485Soft::rxMode()
 int RS485Soft::available()
 {
 	rxMode();
-	return rs485->available();
+	return SoftwareSerial::available();
 }
--- RS485Soft.h
+++ RS485Soft.h
@@ -4,81 +4,56 @@
 // #define RS485_DEBUG				// Enable debug: print incoming bytes
 // #define RS485_DEBUG_TIMESTAMP	// print reading timestamps
 
-#define RS485_SOFT_BUFFER_SIZE 32
 #define RS485_DEFAULT_TIMEOUT 50 // ms
 
 #include "ASCIIDefs.h"
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+#include "RS485Packet.h"
 
 // readChunk() FSM states
-typedef enum
+enum RS485State
 {
 	FSM_WAIT_H0, // Wait for first byte header
 	FSM_WAIT_H1, // Wait for second byte header
+	FSM_SIZE,    // Wait for size byte
 	FSM_PACKET,  // Read packet data until first footer
+	FSM_HASH,	 // Wait for hash byte
 	FSM_WAIT_F1, // Wait for end of packet
 	FSM_END,     // Stop FSM
-} read_fsm_state_t;
+};
 
 
 // readChunk() status codes
-typedef enum
-{
-	NEW_PACKET, // No error
-	NULL_PACKET, // No packet
-	ERROR_TIMEOUT,
-	ERROR_EXCEEDED_NULL_COUNT,
-	ERROR_INCOMPLETE_OR_BROKEN,
-	ERROR_OVERFLOW,
-	ERROR_EMPTY,
-	ERROR_UNKNOWN,
-} read_code_t;
 
-class RS485Soft
+class RS485Soft : public SoftwareSerial
 {
+public:
+	RS485Soft(uint8_t rxPin, uint8_t txPin, uint8_t txControl);
+	~RS485Soft();
+
+	virtual void begin(long speed);
+	virtual int available();
+
+	// packet handling
+	bool readPacket(RS485Packet& packet); // read and save data
+	void send(RS485Packet& packet);
+
 private:
-	SoftwareSerial *rs485;
+	void txMode();
+	void rxMode();
+
 	uint8_t rxPin;
 	uint8_t txPin;
 	uint8_t txControl; // Half-Duplex direction pin
 
 	// timeout handling
-	unsigned long timeout; 	 // timeout in ms
+	unsigned long timeout = RS485_DEFAULT_TIMEOUT; 	 // timeout in ms
 	unsigned long timestamp;
 	void _timeStamp();
 	uint8_t _timedOut();
 	uint8_t errorCode; // last error code
 
-	// packet data
-	uint8_t size;
-	uint8_t buffer[RS485_SOFT_BUFFER_SIZE]; // data chunk
-
-public:
-	RS485Soft(SoftwareSerial &softSerial, uint8_t rxPin, uint8_t txPin, uint8_t txControl);
-	~RS485Soft();
-
-	void begin(long baudrate, unsigned long timeout);
-	void begin(long baudrate);
-
-	int available();
-	void write(uint8_t b);
-	uint8_t read();
-
-	void txMode();
-	void rxMode();
-
-	// chunk handling
-	void sendChunk(uint8_t *data, uint8_t size);
-	void sendChunk(const char *data); // sends NULL terminated string (null is not included in packet)
-	read_code_t readChunk(); // read and buffer chunk of data
-	uint8_t chunkSize();
-	uint8_t chunkData(uint8_t i);
-	uint8_t error(); // get error code
-	void clearChunk();
-
-	// debug
-	void printChunk();
 };
 
 #endif
